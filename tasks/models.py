1875135fac6e771261decf934edea7254901397c
Rudex Tede <rudexjuniortede@gmail.com>
2024-12-06 00:49:16 +0100

    alter task model, add choices for status and priority fields. Create Task Serializer

--- tasks/models.py
+++ tasks/models.py
@@ -1,9 +1,24 @@
 from django.db import models
 
 class Task(models.Model):
+    STATUS_CHOICES = [
+        ('to_do', 'To Do'),
+        ('in_progress', 'In Progress'),
+        ('done', 'Done'),
+    ]
+    
+    PRIORITY_CHOICES = [
+        ('low', 'Low'),
+        ('medium', 'Medium'),
+        ('high', 'High'),
+    ]
+    
     title = models.CharField(max_length=100)
     description = models.TextField()
-    statut = models.CharField(max_length=50)
-    priority = models.CharField(max_length=50)
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='to_do')
+    priority = models.CharField(max_length=50, choices=PRIORITY_CHOICES, default='medium')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"{self.title} - {self.get_status_display()} - {self.get_priority_display()}"
